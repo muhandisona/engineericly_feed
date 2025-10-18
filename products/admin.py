8bd0dc5ed35c0d964c9a562320fa796cc0a9a9c2
Mashkhurbek <mashkhurbek.mashrabov@gmail.com>
2025-10-18 19:27:33 +0500

    fix

--- products/admin.py
+++ products/admin.py
@@ -10,25 +10,28 @@ from products.models import *
 
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
-    list_display = ('title', 'is_pinned', 'created_at')
-    list_display_links = ('title', 'is_pinned', 'created_at')
+    list_display = ('title', 'is_pinned', 'order', 'published_at')
+    list_display_links = ('title', 'is_pinned', 'order', 'published_at')
     search_fields = ('title', 'link')
     list_filter = ('is_pinned', 'show_for_youtube', 'show_for_tiktok', 'show_for_instagram', 'created_at', 'updated_at')
     list_filter_submit = True
     compressed_fields = True
     warn_unsaved_form = True
     list_fullwidth = True
+    ordering = ('-is_pinned', 'order', '-published_at')
 
     fieldsets = (
-        (None, {'fields': ('title', 'link', 'is_pinned',)}),
+        (None, {'fields': ('title', 'link', 'is_pinned', 'order')}),
         ("Social Media", {'fields': ('show_for_youtube', 'show_for_tiktok', 'show_for_instagram')}),
         ("File", {'fields': ('file',)}),
-        ('Timestamps', {'fields': ('created_at', 'updated_at'), 'classes': ('collapse',)}),
+        ('Timestamps', {'fields': ('published_at', 'created_at', 'updated_at'), 'classes': ('collapse',)}),
     )
     add_fieldsets = (
-        (None, {'fields': ('title', 'link', 'is_pinned',)}),
+        (None, {'fields': ('title', 'link', 'is_pinned', 'order')}),
         ("Social Media", {'fields': ('show_for_youtube', 'show_for_tiktok', 'show_for_instagram')}),
         ("File", {'fields': ('file',)}),
+        ('Timestamps', {'fields': ('published_at',)}),
+
     )
     readonly_fields = ('id', 'created_at', 'updated_at')
 
